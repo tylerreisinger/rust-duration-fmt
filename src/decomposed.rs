dd8a6cd75f8bf73b55a4bd43a48bc76d1cb6a357
Tyler Reisinger <reisinger.tyler@gmail.com>
2017-05-28 18:38:12 -0400

    Move fmt::Display impl to decomposed.rs. Restructure fmt module.

--- src/decomposed.rs
+++ src/decomposed.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use std::time;
 
 #[cfg(feature = "float_duration")]
@@ -206,6 +207,35 @@ impl Decompose for time::Duration {
     }
 }
 
+impl fmt::Display for DecomposedTime {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.years() > 0 {
+            write!(f, "{}yr ", self.years())?;
+        }
+        if self.days() > 0 {
+            write!(f, "{}d ", self.days())?;
+        }
+        if self.hours() > 0 || self.days() > 0 || self.years() > 0 {
+            write!(f, "{:02}:", self.hours())?;
+        }
+        write!(f, "{:02}:{:02}", self.minutes(), self.seconds())?;
+
+        if self.nanoseconds() > 0 {
+            write!(f,
+                   ".{:03}'{:03}'{:03}",
+                   self.milliseconds(),
+                   self.microseconds(),
+                   self.nanoseconds())?;
+        } else if self.microseconds() > 0 {
+            write!(f, ".{:03}'{:03}", self.milliseconds(), self.microseconds())?;
+        } else if self.milliseconds() > 0 {
+            write!(f, ".{:03}", self.milliseconds())?;
+        }
+        Ok(())
+    }
+}
+
+
 fn decompose_fractional_seconds(fractional_seconds: f64) -> (u32, u32, u32) {
     let mut rem_frac = fractional_seconds;
 
@@ -286,5 +316,44 @@ mod tests {
                        .with_fractional_seconds(0.00000005)
                        .nanoseconds(),
                    50);
+    }
+
+    #[test]
+    fn test_display() {
+        assert_eq!(format!("{}", FloatDuration::years(2.5).decompose().unwrap()),
+                   "2yr 182d 12:00:00");
+        assert_eq!(format!("{}", FloatDuration::years(1.0).decompose().unwrap()),
+                   "1yr 00:00:00");
+        assert_eq!(format!("{}", FloatDuration::days(2.0).decompose().unwrap()),
+                   "2d 00:00:00");
+        assert_eq!(format!("{}", FloatDuration::minutes(10.0).decompose().unwrap()),
+                   "10:00");
+        assert_eq!(format!("{}", FloatDuration::microseconds(50.0).decompose().unwrap()),
+                   "00:00.000'050");
+        assert_eq!(format!("{}", FloatDuration::seconds(12.5).decompose().unwrap()),
+                   "00:12.500");
+        assert_eq!(format!("{}",
+                           FloatDuration::milliseconds(100.0).decompose().unwrap()),
+                   "00:00.100");
+        assert_eq!(format!("{}", FloatDuration::nanoseconds(10.0).decompose().unwrap()),
+                   "00:00.000'000'010");
+
+        assert_eq!(format!("{}",
+                           (FloatDuration::days(10.0) + FloatDuration::minutes(20.0) +
+                            FloatDuration::seconds(2.0))
+                                   .decompose()
+                                   .unwrap()),
+                   "10d 00:20:02");
+        assert_eq!(format!("{}",
+                           (FloatDuration::seconds(30.5) + FloatDuration::nanoseconds(100.0))
+                               .decompose()
+                               .unwrap()),
+                   "00:30.500'000'100");
+        assert_eq!(format!("{}",
+                           (FloatDuration::seconds(90.0) + FloatDuration::microseconds(500.0))
+                               .decompose()
+                               .unwrap()),
+                   "01:30.000'500");
+
     }
 }
---
+++ src/fmt/lex.rs
@@ -0,0 +1,9 @@
+
+pub struct FmtLexer {
+}
+
+impl FmtLexer {
+    pub fn new() -> FmtLexer {
+        unimplemented!()
+    }
+}
